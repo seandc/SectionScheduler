6c516a09a2df9c3c5171cce463a200620dc39b47
Parker Phinney <parker.phinney@gmail.com>
2012-02-29 17:12:51 -0500

    AUTOCOMMIT: too lazy to write a msg

--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -17,13 +17,16 @@ def availability_form(request, course_id):
     # TODO: figure out if they're a TA
     is_ta = False
 
+    # TODO: figure out if they're male
+    is_male = True
+
     # TODO: get their DND name
     dnd_name = 'Alice'
 
     data = {
         'sections' : course_info['sections'],
         'is_ta' : is_ta,
+        'is_male' : is_male,
         'dnd_name' : dnd_name,
-
         }
     return render_to_response('availability_form.html', data)
